87da94c1bb0b10098607e053645065cb974e9411
Felix Lopez <lfelixfermin@gmail.com>
2022-12-22 18:25:15 -0600

    App test

--- app.config.js
+++ app.config.js
@@ -1,13 +1,15 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "Test",
     "slug": "Test",
     "version": "1.0.0",
     "orientation": "portrait",
-    "icon": "./assets/icon.png",
+    "icon": "./assets/OFI.png",
     "userInterfaceStyle": "light",
     "splash": {
-      "image": "./assets/splash.png",
+      "image": "./assets/OFI.png",
       "resizeMode": "contain",
       "backgroundColor": "#ffffff"
     },
@@ -28,6 +30,14 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket:  process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID
     }
   }
 }
---
+++ assets/src/screens/Home.js
@@ -0,0 +1,40 @@
+import * as React from "react";
+import * as RN from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { database } from "../config/fb";
+import { collection, onSnapshot, orderBy, query, QuerySnapshot } from "firebase/firestore";
+import Product from "../components/Product";
+
+export default function Home() {
+    const navigation = useNavigation();
+    const [products, setProducts] = React.useState([]);
+
+    React.useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => <RN.Button title="Add" onPress={() => navigation.navigate('Add')} />
+        })
+    }, [])
+    React.useEffect(() => {
+        const collectionRef = collection(database, 'products');
+        const q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+        const unsuscribe = onSnapshot(q, QuerySnapshot => {
+            setProducts(
+                QuerySnapshot.docs.map(doc => ({
+                    id: doc.id,
+                    emoji: doc.data().emoji,
+                    name: doc.data().name,
+                    price: doc.data().price,
+                    isSold: doc.data().isSold,
+                    createdAt: doc.data().createdAt,
+                }))
+            )
+        })
+        return unsuscribe;
+    }, [])
+    return (
+        <><RN.Text>Productos</RN.Text>
+            {products.map(product => <Product key={product.id}{...product} />)}
+        </>
+    )
+}
