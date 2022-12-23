5df7008ce16d6bdd2da76e219a88fd8c08aa999c
Felix Lopez <lfelixfermin@gmail.com>
2022-12-22 18:25:26 -0600

    Update

--- app.config.js
+++ app.config.js
@@ -23,6 +23,7 @@ export default {
       "supportsTablet": true
     },
     "android": {
+      "package": "com.fermin13.Test",
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#FFFFFF"
@@ -31,13 +32,20 @@ export default {
     "web": {
       "favicon": "./assets/favicon.png"
     },
+    "extra": {
+      "eas": {
+        "owner": "fermin13",
+        "projectId": "e614ba82-9613-4458-8300-7ee557121afc"
+      }
+    },
     extra: {
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
-      storageBucket:  process.env.STORAGE_BUCKET,
+      storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID
+
     }
   }
 }
